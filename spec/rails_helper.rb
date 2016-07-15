b835e445ef39b057a152d81c264828b16f943dd0
Sergei Silnov <po@kumekay.com>
2016-07-15 09:58:00 +0200

    add api spec autodiscovery and update apispec path, fix ideas api spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,8 @@ require 'json'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -53,7 +54,7 @@ RSpec.configure do |config|
   #       # ...
   #     end
   #
-  # The different available types are documented in the features, such as in
+  # The different available types are documented in the api, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
