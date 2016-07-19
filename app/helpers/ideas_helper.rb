b6dd5f89d7edf6bb0132c11bd5cb1323f3e739ef
Egor G. Silaev <silaevegor@hotmail.com>
2016-07-19 21:00:22 +0300

    Add tagging to the project
    Add 2 new models tag and tagging. Modified idea views.
    Remove home controller, add helpers for tag. Add ajax

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -5,7 +5,6 @@ module IdeasHelper
 
   def tag_cloud(tags, classes)
     max = tags.sort_by(&:count).last
-    puts max
     tags.each do |tag|
       index = tag.count.to_f / max.count * (classes.size-1)
       yield(tag, classes[index.round])
