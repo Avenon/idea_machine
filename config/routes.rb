9a9a2b4abff9b9b5564e1b218f96358e88115689
Egor G. Silaev <silaevegor@hotmail.com>
2016-07-19 16:52:43 +0300

    Remove home controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   mount API::Root => '/'
   use_doorkeeper
-  use_doorkeeper
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  root to: "home#index"
+  root to: "ideas#index"
   resources :ideas
   resources :users
   resources :projects
