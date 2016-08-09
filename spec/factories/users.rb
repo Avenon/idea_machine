8862d21e99cd2a47a596d1f9559377296e516e37
Egor G. Silaev <silaevegor@hotmail.com>
2016-08-09 12:16:14 +0300

    Add feature spec within poltergeist

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "test@test.ru"
+    password "test1234"
+  end
+end
--- spec/features/ajax_spec.rb
+++ spec/features/ajax_spec.rb
@@ -1,13 +1,17 @@
 require "rails_helper"
 
 describe 'Check ajax' do
+  #let!(:user) { create(:user, email: "test@test.ru", password: "test", password_confirmation: "test") }
+  #let!(:card) { FactoryGirl.create(:card, user_id: user.id) }
+  let!(:user) { FactoryGirl.create(:user) }
 
   it "Card should be create with ajax" do
     visit root_path
-    #click_link I18n.t :sign_in
-    #fill_in :email, with: "test@test.ru"
-    #fill_in :password, with: "test"
-    #click_button I18n.t :sign_in
+    click_link "Sign in"
+    fill_in :user_email, with: "test@test.ru"
+    fill_in :user_password, with: "test1234"
+    click_button "Log in"
+
     #click_link I18n.t :add_card
     expect(page.status_code).to eq(200)
   end
